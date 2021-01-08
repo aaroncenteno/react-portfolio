c13a912de84be06745252ceafc6261356c52a8f9
ASC8 <asamcent@gmail.com>
2021-01-07 23:43:13 -0800

    ternary statement for current page added to App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import Nav from './components/Nav';
 
 function App() {
   const [contactSelected, setContactSelected] = useState(false);
-  
+  const pages = ['about', 'portfolio', 'contact', 'resume'];
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
+  function ternaryNav() {
+    return (
+      <main>
+        {(currentPage === 'portfolio') ? (
+          <Portfolio></Portfolio>
+        ) : (currentPage === 'contact') ? (
+          <Contact></Contact>
+        ) : (currentPage === 'resume') ? (
+          <Resume></Resume>
+        ) : (
+          <About></About>
+        )}
+      </main>
+    )
+  }
 
   return (
     <div>
