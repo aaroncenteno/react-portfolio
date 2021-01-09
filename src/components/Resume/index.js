0159e9c2dcdb71b873164762d6e6f07fe73224d6
ASC8 <asamcent@gmail.com>
2021-01-08 19:16:46 -0800

    resume page added and styled with mobile responsiveness

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,46 @@
 import React, { useEffect } from 'react';
+import resumePdf from '../../assets/resume/AC_Resume08122020.pdf'
 
 function Resume () {
     return (
-        <div>
-            
-        </div>
+        <section className='resume-section'>
+			<div className='resume-container'>
+				<h1>Resumé</h1>
+				<p>
+					Download my<span> </span>
+					<a
+						href={resumePdf}
+						target='_blank'
+                        className="resume-download">
+						Resumé
+					</a>
+				</p>
+				<br />
+
+				<h2>Front End</h2>
+				<ul>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JavaScript</li>
+					<li>jQuery</li>
+					<li>React</li>
+					<li>Bootstrap</li>
+                    <li>Chart.js</li>
+                    <li>Responsive Design</li>
+				</ul>
+				<br/>
+
+				<h2>Back End</h2>
+				<ul>
+					<li>APIs</li>
+					<li>Node</li>
+					<li>Express</li>
+					<li>MySQL, Sequelize</li>
+					<li>MongoDB, Mongoose</li>
+					<li>REST</li>
+				</ul>
+			</div>
+		</section>
     )
 }
 
