4fe3c7bbab957e5c2fc70bbcc0232f5bdaa1820c
ASC8 <asamcent@gmail.com>
2021-02-07 00:30:54 -0800

    Revert "fix react-router bug"
    This reverts commit c5d29973ccc8022714633d4da8ec8c5d9ba8aa09.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,7 +4,7 @@ import covidCa from '../../assets/small/covid.jpg'
 import fitnessPal from '../../assets/small/fitness-pal-screenshot.jpg'
 import weatherDash from '../../assets/small/weather-dashboard.jpg'
 import codingQuiz from '../../assets/small/coding-quiz.jpg'
-import runBuddy from '../../assets/small/Run-Buddy.jpg'
+import wanderPro from '../../assets/small/wanderPro.png'
 import techBlog from '../../assets/small/techBlog_dashboard.jpg'
 
 
@@ -12,10 +12,10 @@ function Portfolio(props) {
     return (
         <div className='portfolio-container'>
             <Work 
-                name='Covid CA'
-                repoLink='https://github.com/aaroncenteno/covid-ca'
-                liveLink='https://aaroncenteno.github.io/covid-ca/'
-                projectImage={covidCa}
+                name='WanderPro'
+                repoLink='https://github.com/aaroncenteno/wanderpro'
+                liveLink='http://wanderpro.herokuapp.com/'
+                projectImage={wanderPro}
                 className='work-item'
             />
             <Work 
@@ -23,6 +23,13 @@ function Portfolio(props) {
                 repoLink='https://github.com/aaroncenteno/fitness-pal'
                 liveLink='https://fitness-pal-jhacms.herokuapp.com/'
                 projectImage={fitnessPal}
+                className='work-item'
+            />
+            <Work 
+                name='Covid CA'
+                repoLink='https://github.com/aaroncenteno/covid-ca'
+                liveLink='https://aaroncenteno.github.io/covid-ca/'
+                projectImage={covidCa}
                 className='work-item'
             />
             <Work 
@@ -44,13 +51,6 @@ function Portfolio(props) {
                 repoLink='https://github.com/aaroncenteno/web-api-code-quiz'
                 liveLink='https://aaroncenteno.github.io/web-api-code-quiz/'
                 projectImage={codingQuiz}
-                className='work-item'
-            />
-            <Work 
-                name='Run Buddy'
-                repoLink='https://github.com/aaroncenteno/run-buddy'
-                liveLink='https://aaroncenteno.github.io/run-buddy/'
-                projectImage={runBuddy}
                 className='work-item'
             />
         </div>
